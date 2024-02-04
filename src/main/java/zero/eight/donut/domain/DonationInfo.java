d2c96397179ddbcdc7742c52f69b2370178d0f4e
kang <kang6@ewhain.net>
2024-02-04 21:10:42 +0900

    feat: created DonationInfo Entity

---
+++ src/main/java/zero/eight/donut/domain/DonationInfo.java
@@ -0,0 +1,24 @@
+package zero.eight.donut.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DonationInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "donationInfo_id")
+    private Long id;
+
+    @Column
+    private Long sum;
+
+    @Column
+    private Long count;
+
+}
