064c8331b7a9ff85e6eb7e5b836b89cb6444b2bd
kang <kang6@ewhain.net>
2024-02-04 17:04:49 +0900

    chore: 초기 세팅

---
+++ src/main/java/zero/eight/donut/DonutApplication.java
@@ -0,0 +1,13 @@
+package zero.eight.donut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zero/eight/donut/DonutApplicationTests.java
@@ -0,0 +1,13 @@
+package zero.eight.donut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
