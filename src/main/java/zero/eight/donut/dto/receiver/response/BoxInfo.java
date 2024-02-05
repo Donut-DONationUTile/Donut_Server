894d0dfa33c43c641ea967d722699a9fff764b32
kang <kang6@ewhain.net>
2024-02-06 06:31:46 +0900

    chore: change attribute name deadline to dueDate

--- src/main/java/zero/eight/donut/dto/receiver/response/BoxInfo.java
+++ src/main/java/zero/eight/donut/dto/receiver/response/BoxInfo.java
@@ -1,20 +1,22 @@
 package zero.eight.donut.dto.receiver.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class BoxInfo {
     private Long boxId;
     private String store;
-    private LocalDateTime deadline;
+    private LocalDateTime dueDate;
     private Integer amount;
 
     @Builder
-    public BoxInfo(Long boxId, String store, LocalDateTime deadline, Integer amount){
+    public BoxInfo(Long boxId, String store, LocalDateTime dueDate, Integer amount){
         this.boxId = boxId;
         this.store = store;
-        this.deadline = deadline;
+        this.dueDate = dueDate;
         this.amount = amount;
     }
 }
