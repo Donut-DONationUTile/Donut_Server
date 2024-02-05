19868382fd963029ca17bf0b1ed723ba3eb129dc
kang <kang6@ewhain.net>
2024-02-06 03:34:57 +0900

    refactor: delete unused code

--- src/main/java/zero/eight/donut/repository/GiverRepository.java
+++ src/main/java/zero/eight/donut/repository/GiverRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import zero.eight.donut.domain.Giver;
 
 public interface GiverRepository extends JpaRepository <Giver, Long> {
-    Integer countBy();
 }
