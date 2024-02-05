2ff9b81400f5f8dc0aeb00966528cc42b6fce361
kang <kang6@ewhain.net>
2024-02-06 07:44:40 +0900

    chore: Todo comment

--- src/main/java/zero/eight/donut/repository/GiftboxRepository.java
+++ src/main/java/zero/eight/donut/repository/GiftboxRepository.java
@@ -1,6 +1,7 @@
 package zero.eight.donut.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import zero.eight.donut.domain.Giftbox;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface GiftboxRepository extends JpaRepository<Giftbox, Long> {
+    //@Query("SELECT gb.sum FROM Giftbox gb WHERE gb.receiver = ?1 AND gb.= ?2")
     List<Giftbox> findAllByReceiverId(Long receiver_id);
 }
--- src/main/java/zero/eight/donut/service/ReceiverService.java
+++ src/main/java/zero/eight/donut/service/ReceiverService.java
@@ -1,6 +1,7 @@
 package zero.eight.donut.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import zero.eight.donut.domain.Giftbox;
@@ -8,6 +9,9 @@ import zero.eight.donut.domain.Receiver;
 import zero.eight.donut.dto.receiver.response.BoxInfo;
 import zero.eight.donut.dto.receiver.response.ReceiverGetBoxResponseDto;
 import zero.eight.donut.dto.receiver.response.ReceiverHomeResponseDto;
+import zero.eight.donut.exception.Error;
+import zero.eight.donut.exception.NotFoundException;
+import zero.eight.donut.repository.GiftRepository;
 import zero.eight.donut.repository.GiftboxRepository;
 
 import java.util.List;
@@ -16,9 +20,13 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ReceiverService {
+    private final GiftRepository giftRepository;
     private final GiftboxRepository giftboxRepository;
     @Transactional
     public ReceiverHomeResponseDto receiverHome(Receiver receiver){
+        /***
+         * 활성화된 꾸러미만 조회하도록 변경
+         */
         List<Giftbox> giftboxList = giftboxRepository.findAllByReceiverId(receiver.getId());
 
         Long amount = giftboxList.stream().mapToLong(boxInfo -> boxInfo.getAmount()).sum();
@@ -36,7 +44,6 @@ public class ReceiverService {
        ***/
         Boolean availability  = true;
 
-
         return ReceiverHomeResponseDto.builder()
                 .availability(availability)
                 .amount(amount)
@@ -47,6 +54,9 @@ public class ReceiverService {
 
     @Transactional
     public ReceiverGetBoxResponseDto receiverGetOneBox(Receiver receiver, Long boxId){
+        Giftbox giftbox = giftboxRepository.findById(boxId)
+                .orElseThrow(() -> new NotFoundException(Error.GIFTBOX_NOT_FOUND_EXCEPTION));
+
         return ReceiverGetBoxResponseDto.builder().build();
     }
 
