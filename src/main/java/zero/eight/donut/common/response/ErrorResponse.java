8db667b776e8fda4f84207317cb51307cf5f3bc8
kang <kang6@ewhain.net>
2024-02-06 03:46:00 +0900

    feat: change response format

--- src/main/java/zero/eight/donut/common/response/ErrorResponse.java
+++ src/main/java/zero/eight/donut/common/response/ErrorResponse.java
@@ -9,15 +9,16 @@ import lombok.ToString;
 @ToString
 @RequiredArgsConstructor
 public class ErrorResponse {
-    private final int code;
+    private final int status;
+    private final boolean success;
     private final String message;
 
     public static ErrorResponse error(Error error) {
-        return new ErrorResponse(error.getHttpStatusCode(), error.getMessage());
+        return new ErrorResponse(error.getHttpStatusCode(), error.getHttpStatus().is2xxSuccessful(), error.getMessage());
     }
 
 
     public static ErrorResponse error(Error error, String message) {
-        return new ErrorResponse(error.getHttpStatusCode(), message);
+        return new ErrorResponse(error.getHttpStatusCode(), error.getHttpStatus().is2xxSuccessful(), message);
     }
 }
--- src/main/java/zero/eight/donut/common/response/SuccessResponse.java
+++ src/main/java/zero/eight/donut/common/response/SuccessResponse.java
@@ -8,16 +8,17 @@ import zero.eight.donut.exception.Success;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SuccessResponse<T> {
-    private final int code;
+    private final int status;
+    private final boolean success;
     private final String message;
     private T data;
 
     public static SuccessResponse success(Success success) {
-        return new SuccessResponse<>(success.getHttpStatusCode(), success.getMessage());
+        return new SuccessResponse<>(success.getHttpStatusCode(), success.getHttpStatus().is2xxSuccessful(), success.getMessage());
     }
 
 
     public static <T> SuccessResponse<T> success(Success success, T data) {
-        return new SuccessResponse<T>(success.getHttpStatusCode(), success.getMessage(), data);
+        return new SuccessResponse<T>(success.getHttpStatusCode(),  success.getHttpStatus().is2xxSuccessful(),success.getMessage(), data);
     }
 }
