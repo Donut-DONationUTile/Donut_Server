2e0382916d4d0cd98721c505c894f632fa90e011
kang <kang6@ewhain.net>
2024-02-06 20:04:57 +0900

    chore: change http status code

--- src/main/java/zero/eight/donut/exception/NotFoundException.java
+++ src/main/java/zero/eight/donut/exception/NotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+@ResponseStatus(NOT_FOUND)
 public class NotFoundException extends ApiException {
     public NotFoundException(Error error) {
         super(error);
