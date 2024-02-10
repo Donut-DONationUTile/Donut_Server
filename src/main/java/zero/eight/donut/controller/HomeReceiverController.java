5cb492897f0e883a98312e985add4c0d6a7c2e48
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-02-11 00:10:49 +0900

    chore: resolve merge conflicts
    - Success에서 누락된 객체를 추가하였습니다.
    - HomeReceiverController에서 @LoginUser를 JWT에 맞게 대체하였습니다.

--- src/main/java/zero/eight/donut/controller/HomeReceiverController.java
+++ src/main/java/zero/eight/donut/controller/HomeReceiverController.java
@@ -6,23 +6,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import zero.eight.donut.common.response.SuccessResponse;
-import zero.eight.donut.config.annotation.LoginUser;
 import zero.eight.donut.domain.Receiver;
 import zero.eight.donut.dto.home.receiver.ReceiverGetBoxResponseDto;
 import zero.eight.donut.dto.home.receiver.ReceiverGetGiftResponseDto;
 import zero.eight.donut.dto.home.receiver.ReceiverHomeResponseDto;
 import zero.eight.donut.exception.Success;
+import zero.eight.donut.service.AuthUtils;
 import zero.eight.donut.service.HomeReceiverService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/home/receiver")
 public class HomeReceiverController {
-    private  final HomeReceiverService homeReceiverService;
+    private final HomeReceiverService homeReceiverService;
+    private final AuthUtils authUtils;
 
     @GetMapping("")
-    public SuccessResponse<ReceiverHomeResponseDto> receiverHome(@LoginUser Receiver receiver){
-        return SuccessResponse.success(Success.HOME_RECEIVER_SUCCESS, homeReceiverService.receiverHome(receiver));
+    public SuccessResponse<ReceiverHomeResponseDto> receiverHome(){
+        return SuccessResponse.success(Success.HOME_RECEIVER_SUCCESS, homeReceiverService.receiverHome(authUtils.getReceiver()));
     }
 
     @GetMapping("/box/{boxId}")
