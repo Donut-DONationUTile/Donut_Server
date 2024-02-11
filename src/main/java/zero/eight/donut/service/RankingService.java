56e4722b7d9f5deb6dd1b84b0feb6f09bf8f06b9
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-02-11 16:09:31 +0900

    fix: 기부 랭킹 배열 인덱스 오류 해결
    - 기부 랭킹 조회 시, 잘못된 인덱스 호출로 인한 IndexOutOfBoundsException을 해결하였습니다.

--- src/main/java/zero/eight/donut/service/RankingService.java
+++ src/main/java/zero/eight/donut/service/RankingService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import zero.eight.donut.common.response.ApiResponse;
 import zero.eight.donut.domain.Donation;
 import zero.eight.donut.domain.Giver;
@@ -24,7 +25,13 @@ public class RankingService {
 
     private final DonationRepository donationRepository;
     private final AuthUtils authUtils;
+
+    @Transactional
     public ApiResponse<?> getRankingByPrice() {
+
+        log.info("role -> {}", authUtils.getCurrentUserRole());
+        log.info("ROLE_GIVER -> {}", Role.ROLE_GIVER);
+
         // 기부자인지 역할 조회
         if (!authUtils.getCurrentUserRole().equals(Role.ROLE_GIVER)) {
             return ApiResponse.failure(Error.NOT_AUTHENTICATED_EXCEPTION);
@@ -35,18 +42,27 @@ public class RankingService {
         // 기부 금액 내림차순으로 기부 내역 조회
         List<Donation> donationList = donationRepository.findAll(Sort.by(Sort.Direction.DESC, "sum"));
         for (int idx = 1; idx <= donationList.size(); idx++) {
-            Donation d = donationList.get(idx);
+            Donation d = donationList.get(idx - 1);
             Giver giver = d.getGiver();
             if (giver == null) { // 기부자 부재 시 서버 exception error
                 return ApiResponse.failure(Error.INTERNAL_SERVER_ERROR);
             }
-            rankingList.add(new RankingByPriceResponseDto(idx, giver.getName(), d.getSum()));
+            rankingList.add(RankingByPriceResponseDto.builder()
+                            .rank(idx)
+                            .name(giver.getName())
+                            .price(d.getSum())
+                            .build());
         }
 
         return ApiResponse.success(Success.GET_RANKING_BY_PRICE_SUCCESS, rankingList);
     }
 
+    @Transactional
     public ApiResponse<?> getRankingByNumber() {
+
+        log.info("role -> {}", authUtils.getCurrentUserRole());
+        log.info("ROLE_GIVER -> {}", Role.ROLE_GIVER);
+
         // 기부자인지 역할 조회
         if (!authUtils.getCurrentUserRole().equals(Role.ROLE_GIVER)) {
             return ApiResponse.failure(Error.NOT_AUTHENTICATED_EXCEPTION);
@@ -57,12 +73,16 @@ public class RankingService {
         // 기부 횟수 내림차순으로 기부 내역 조회
         List<Donation> donationList = donationRepository.findAll(Sort.by(Sort.Direction.DESC, "count"));
         for (int idx = 1; idx <= donationList.size(); idx++) {
-            Donation d = donationList.get(idx);
+            Donation d = donationList.get(idx - 1);
             Giver giver = d.getGiver();
             if (giver == null) { // 기부자 부재 시 서버 exception error
                 return ApiResponse.failure(Error.INTERNAL_SERVER_ERROR);
             }
-            rankingList.add(new RankingByNumberResponseDto(idx, giver.getName(), d.getCount()));
+            rankingList.add(RankingByNumberResponseDto.builder()
+                            .rank(idx)
+                            .name(giver.getName())
+                            .number(d.getCount())
+                            .build());
         }
 
         return ApiResponse.success(Success.GET_RANKING_BY_NUMBER_SUCCESS, rankingList);
