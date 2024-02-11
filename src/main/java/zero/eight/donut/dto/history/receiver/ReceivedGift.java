7ebc99041c32fd12b48a094d2213c5193d513f1c
kang <kang6@ewhain.net>
2024-02-12 00:39:27 +0900

    chore: change name of dto

--- src/main/java/zero/eight/donut/dto/history/receiver/ReceivedGift.java
+++ src/main/java/zero/eight/donut/dto/history/receiver/ReceivedGift.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
-public class Gift {
+public class ReceivedGift {
     private Long giftId;
     private String product;
     private Integer price;
