185815e7cc6a52af97be669b245a7039d31e2ee8
kang <kang6@ewhain.net>
2024-02-11 23:06:23 +0900

    feat: create response dto for get received donation list

---
+++ src/main/java/zero/eight/donut/dto/history/receiver/Gift.java
@@ -0,0 +1,17 @@
+package zero.eight.donut.dto.history.receiver;
+
+import lombok.Builder;
+import lombok.Getter;
+import zero.eight.donut.domain.enums.Status;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class Gift {
+    private Long giftId;
+    private String product;
+    private Integer price;
+    private LocalDateTime dueDate;
+    private Status isUsed;
+}
