73985c3f695bb501edd6e2d6742fd3b752426099
kang <kang6@ewhain.net>
2024-02-13 23:14:04 +0900

    feat: create toEntity method

--- src/main/java/zero/eight/donut/dto/donation/DonateGiftRequestDto.java
+++ src/main/java/zero/eight/donut/dto/donation/DonateGiftRequestDto.java
@@ -3,6 +3,10 @@ package zero.eight.donut.dto.donation;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
+import zero.eight.donut.domain.Gift;
+import zero.eight.donut.domain.Giftbox;
+import zero.eight.donut.domain.Giver;
+import zero.eight.donut.domain.enums.Status;
 import zero.eight.donut.domain.enums.Store;
 
 import java.time.LocalDateTime;
@@ -15,4 +19,19 @@ public class DonateGiftRequestDto {
     private Integer price;
     private LocalDateTime dueDate;
     private Store store;
+
+    public Gift toEntity(Giver giver, Giftbox giftbox, String imageUrl){
+        return Gift.builder()
+                .giver(giver)
+                .product(product)
+                .isAssigned(false)
+                .isMsgReceived(false)
+                .status(Status.UNUSED)
+                .price(price)
+                .imageUrl(imageUrl)
+                .store(store)
+                .dueDate(dueDate)
+                .giftbox(giftbox)
+                .build();
+    }
 }
