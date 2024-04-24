0a72d99c6ba804499063301314cac394ced8bfca
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-04-25 03:31:14 +0900

    feat: 월렛 화면 조회 구현
    - reponse message를 커스텀하였습니다.
    - 보유 기프티콘이 있을 때만 계산 로직을 수행하도록 조정하였습니다.
    - 주석문 정리로 가독성을 높였습니다.

--- src/main/java/zero/eight/donut/service/WalletService.java
+++ src/main/java/zero/eight/donut/service/WalletService.java
@@ -30,54 +30,59 @@ public class WalletService {
     private final ReceiverRepository receiverRepository;
     private final GiftRepository giftRepository;
 
-    public ApiResponse<?> walletMain() { // 월렛 화면 조회
-        // 조회 시각
-        LocalDateTime now = LocalDateTime.now();
-        // 기부자 정보
-        Giver giver = authUtils.getGiver();
-        // 수혜자 수
-        int receiver = receiverRepository.countBy();
-        // 기프티콘 총액
-        double amount = 0d;
+    // 월렛 화면 조회
+    public ApiResponse<?> walletMain() {
+        // 변수 선언
+        LocalDateTime now = LocalDateTime.now(); // 조회 시각
+        Giver giver = authUtils.getGiver(); // 기부자 정보
+        int receiver = receiverRepository.countBy(); // 수혜자 수
+        double amount = 0d; // 기프티콘 총액
+        int cu = 0; // 보유 기프티콘 개수(CU)
+        int gs25 = 0; // 보유 기프티콘 개수(GS25)
+        int seveneleven = 0; // 보유 기프티콘 개수(7eleven)
+        List<WalletGiftInfoDto> impendingList = new ArrayList<>(); // 임박 기프티콘 리스트
+        List<WalletGiftInfoDto> giftList = new ArrayList<>(); // 일반 기프티콘 리스트
+        
         // 월렛 기프티콘 리스트 조회
         List<Gift> targetList = giftRepository.findAllByGiverAndStatusAndDueDateAfterOrToday(giver.getId(), now);
-        // 사용처별 기프티콘 개수
-        Map<Store, Long> giftCountMap = countGiftsByStore(targetList);
-        int cu = Math.toIntExact(giftCountMap.get(Store.CU));
-        int gs25 = Math.toIntExact(giftCountMap.get(Store.GS25));
-        int seveneleven = Math.toIntExact(giftCountMap.get(Store.SEVENELEVEN));
 
-        // 기한별 기프티콘 리스트
-        List<WalletGiftInfoDto> impendingList = new ArrayList<>();
-        List<WalletGiftInfoDto> giftList = new ArrayList<>();
-
-        for (Gift g : targetList) {
-            // 기프티콘 총액 계산
-            amount += g.getPrice();
-            // 기프티콘 분류
-            if (g.getDueDate().isBefore(LocalDateTime.now().plusDays(30))) { // 유효 기간 30일 이내
-                impendingList.add(
-                        WalletGiftInfoDto.builder()
-                                .giftId(g.getId())
-                                .days(Math.toIntExact(Duration.between(now, g.getDueDate()).toDaysPart()))
-                                .store(g.getStore().getStore())
-                                .dueDate(g.getDueDate())
-                                .product(g.getProduct())
-                                .price(g.getPrice())
-                                .build()
-                );
-            }
-            else { // 유효 기간 30일 이후
-                giftList.add(
-                        WalletGiftInfoDto.builder()
-                                .giftId(g.getId())
-                                .days(Math.toIntExact(Duration.between(now, g.getDueDate()).toDaysPart()))
-                                .store(g.getStore().getStore())
-                                .dueDate(g.getDueDate())
-                                .product(g.getProduct())
-                                .price(g.getPrice())
-                                .build()
-                );
+        if (targetList != null || !targetList.isEmpty()) { // 보유 기프티콘이 있음
+            // 사용처별 기프티콘 개수 계산
+            Map<Store, Long> giftCountMap = countGiftsByStore(targetList);
+            cu = Math.toIntExact(giftCountMap.get(Store.CU));
+            gs25 = Math.toIntExact(giftCountMap.get(Store.GS25));
+            seveneleven = Math.toIntExact(giftCountMap.get(Store.SEVENELEVEN));
+            
+            for (Gift g : targetList) {
+                // 기프티콘 총액 계산
+                amount += g.getPrice();
+                // 기프티콘 분류
+                if (g.getDueDate().isBefore(LocalDateTime.now().plusDays(30))) { // 유효 기간 30일 이내
+                    // 임박 리스트에 할당
+                    impendingList.add(
+                            WalletGiftInfoDto.builder()
+                                    .giftId(g.getId())
+                                    .days(Math.toIntExact(Duration.between(now, g.getDueDate()).toDaysPart()))
+                                    .store(g.getStore().getStore())
+                                    .dueDate(g.getDueDate())
+                                    .product(g.getProduct())
+                                    .price(g.getPrice())
+                                    .build()
+                    );
+                }
+                else { // 유효 기간 30일 이후
+                    // 일반 리스트에 할당
+                    giftList.add(
+                            WalletGiftInfoDto.builder()
+                                    .giftId(g.getId())
+                                    .days(Math.toIntExact(Duration.between(now, g.getDueDate()).toDaysPart()))
+                                    .store(g.getStore().getStore())
+                                    .dueDate(g.getDueDate())
+                                    .product(g.getProduct())
+                                    .price(g.getPrice())
+                                    .build()
+                    );
+                }
             }
         }
 
@@ -92,7 +97,7 @@ public class WalletService {
                 .giftList(giftList)
                 .build();
 
-        return ApiResponse.success(Success.SUCCESS, responseDto);
+        return ApiResponse.success(Success.GET_WALLET_SUCCESS, responseDto);
     }
 
     // 사용처(store) 필드의 값마다 gift 개수 세기
