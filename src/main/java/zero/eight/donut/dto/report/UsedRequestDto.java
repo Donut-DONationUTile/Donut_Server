85e5a4e669d72620a81d4e0823d1953ffcd37a47
kang <kang6@ewhain.net>
2024-04-25 00:32:02 +0900

    chore: rename Report to Used

--- src/main/java/zero/eight/donut/dto/report/UsedRequestDto.java
+++ src/main/java/zero/eight/donut/dto/report/UsedRequestDto.java
@@ -1,13 +1,12 @@
 package zero.eight.donut.dto.report;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ReportRequestDto {
+public class UsedRequestDto {
     private Long giftId;
 }
