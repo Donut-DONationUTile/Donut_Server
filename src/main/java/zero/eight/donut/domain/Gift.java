88b4e3c5250cf3e435cec908619eee88ad62a69b
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-04-28 02:45:26 +0900

    feat: Gift 엔티티 변경
    - @ColumnDefault(값이 없을 때 기본값 삽입)와 @DynamicInsert(NULL일 때 값을 없앰)를 추가하여 defaul value를 설정하였습니다(isAssigned, isMsgReceived, autoDonation).
    - 자동 기부 허용 여부를 나타내는 필드 autoDonation을 추가하였습니다.

--- src/main/java/zero/eight/donut/domain/Gift.java
+++ src/main/java/zero/eight/donut/domain/Gift.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import zero.eight.donut.common.domain.BaseTimeEntity;
 import zero.eight.donut.domain.enums.Status;
 import zero.eight.donut.domain.enums.Store;
@@ -16,6 +18,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert
 public class Gift extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +29,11 @@ public class Gift extends BaseTimeEntity {
     private String product; // 상품명
 
     @Column
+    @ColumnDefault("false")
     private Boolean isAssigned; // 할당여부
 
     @Column
+    @ColumnDefault("false")
     private Boolean isMsgReceived; // 신규 메세지 등록 여부
 
     @Enumerated(value = EnumType.STRING)
@@ -57,6 +62,10 @@ public class Gift extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "giftbox_id")
     private Giftbox giftbox; // 꾸러미 고유 ID
+
+    @Column(name = "auto_danation")
+    @ColumnDefault("false")
+    private Boolean autoDonation; // 자동 기부 허용
 
     //신고 -> 단방향으로 수정
 //    @OneToOne(fetch = FetchType.LAZY)
