1a7490f89466b86de92b69878ebfe85cdd7027dd
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-04-28 03:18:05 +0900

    chore: 경로 변경
    - 직관적인 이해와 코드 복잡도를 고려하여 경로를 변경하였습니다.

--- src/main/java/zero/eight/donut/controller/WalletController.java
+++ src/main/java/zero/eight/donut/controller/WalletController.java
@@ -2,7 +2,6 @@ package zero.eight.donut.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import zero.eight.donut.common.response.ApiResponse;
 import zero.eight.donut.dto.wallet.WalletUploadRequestDto;
 import zero.eight.donut.service.WalletService;
@@ -16,7 +15,7 @@ public class WalletController {
 
     private final WalletService walletService;
 
-    @GetMapping("/main")
+    @GetMapping
     public ApiResponse<?> walletMain() {
         return walletService.walletMain();
     }
@@ -26,7 +25,7 @@ public class WalletController {
         return walletService.walletDetail(giftId);
     }
 
-    @PostMapping("/upload")
+    @PostMapping
     public ApiResponse<?> walletUpload(@RequestBody WalletUploadRequestDto requestDto) throws IOException {
         return walletService.walletUpload(requestDto);
     }
