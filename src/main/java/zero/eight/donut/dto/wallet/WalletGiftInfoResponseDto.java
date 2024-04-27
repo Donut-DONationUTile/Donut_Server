77899fe751053d5bac5f3e30b765486679616cba
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-04-28 02:35:07 +0900

    style: 클래스 이름 변경
    - DTO 클래스의 이름을 직관적으로 변경하였습니다.

--- src/main/java/zero/eight/donut/dto/wallet/WalletGiftInfoResponseDto.java
+++ src/main/java/zero/eight/donut/dto/wallet/WalletGiftInfoResponseDto.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
-public class WalletGiftInfoDto {
+public class WalletGiftInfoResponseDto {
     private long giftId; // 기프티콘 고유 ID
     private int days; // 디데이 남은 일수
     private String store; // 사용처
