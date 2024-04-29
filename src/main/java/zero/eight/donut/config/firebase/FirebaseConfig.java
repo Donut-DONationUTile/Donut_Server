bfde3fc627cf8ea5d18f8bd5aea05f7e528a6a04
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-04-30 04:16:04 +0900

    feat: set firebase configuration
    - 파이어베이스 기본 설정을 완료하였습니다.

---
+++ src/main/java/zero/eight/donut/config/firebase/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package zero.eight.donut.config.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
