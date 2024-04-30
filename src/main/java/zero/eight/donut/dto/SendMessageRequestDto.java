13d4edcece033acd3d31464ed3f5331e921effb4
kang <kang6@ewhain.net>
2024-05-01 05:23:38 +0900

    feat: send msg if gift used

--- src/main/java/zero/eight/donut/dto/SendMessageRequestDto.java
+++ src/main/java/zero/eight/donut/dto/SendMessageRequestDto.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class SendMessageRequestDto {
-    private Long boxId; // 꾸러미 고유 ID
+    private Long giftId; // 기프티콘 고유 ID
     private String content; // 메세지 본문
 }
