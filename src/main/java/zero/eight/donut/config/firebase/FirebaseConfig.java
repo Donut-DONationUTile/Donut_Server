8584126435c15defcef1071672cd4728c5f29815
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-05-01 01:48:33 +0900

    fix: Revert commit

--- src/main/java/zero/eight/donut/config/firebase/FirebaseConfig.java
+++ src/main/java/zero/eight/donut/config/firebase/FirebaseConfig.java
@@ -4,13 +4,10 @@ import com.google.api.gax.core.CredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.util.List;
 
 @Configuration
@@ -48,10 +45,5 @@ public class FirebaseConfig {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Bean
-    public FirebaseMessaging firebaseMessaging() {
-        return FirebaseMessaging.getInstance();
     }
 }
